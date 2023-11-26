55012c002bf0bb530caf5a6bd4edb758ea068033
Vitaliy Derda <derdavitaliy@gmail.com>
2023-11-26 10:54:46 +0200

    feat: added order controller and views

---
+++ Web/ClientApp/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientApp.Controllers;
+
+public class OrderController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Details()
+    {
+        return View();
+    }
+}
